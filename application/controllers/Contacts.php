ae930f2b6f037859dce5fa19004b83738fc1778d
sabinacolaco <78034115+sabinacolaco@users.noreply.github.com>
2021-01-27 19:52:11 +0800

    Contacts list page
    Changed the base url
    Added curl library for API request
    Changed the default route to contact list page
    Added the functionality for the contact list page

---
+++ application/controllers/Contacts.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contacts extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+    {
+        $this->load->library('curl');
+        $result = $this->curl->simple_get('https://demomedia.co.uk/files/contacts.json');
+        $result = json_decode($result, true);
+
+        $data['tableRows'] = $result;
+        $this->load->view('contact_list', $data);
+    }
+}
